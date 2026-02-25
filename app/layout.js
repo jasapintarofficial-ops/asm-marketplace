ed5bbfb91b25aaa45458e3c003341a2ee4785e9b
jasapintarofficial-ops <jasapintar.official@gmail.com>
2026-02-25 19:16:07 +0700

    layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'JasaPintar - Platform Jasa Profesional',
+  description: 'Temukan tukang AC, sedot WC, sumur bor terpercaya di kota Anda',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  )
+}
