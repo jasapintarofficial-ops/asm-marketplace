f206fcf25ee5bdfb81853e68c0249047ebabb501
jasapintarofficial-ops <jasapintar.official@gmail.com>
2026-02-26 13:29:31 +0700

    Delete app/lib directory

--- app/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://qxvawdvddqogwntfpmtq.supabase.co'
-const supabaseKey = 'sb_publishable_vpKZ-o_POYCsPxk398F6jg_AJaOgsFU'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
